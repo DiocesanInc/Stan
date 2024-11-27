33d3e5374c766979e5cd0a1f38734d88152bf35b
marcus-borger <57198057+marcus-borger@users.noreply.github.com>
2024-11-27 10:45:12 -0500

    Fix GF Submit Button, also fix issues in scss files

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -137,65 +137,72 @@ function getMinistryPostObject()
 if (!function_exists("my_form_submit_button")) {
     function my_form_submit_button($button, $form)
     {
-        return "<button type='submit' class='the-button' id='gform_submit_button_{$form['id']}'>Submit</button>";
+        $fragment = WP_HTML_Processor::create_fragment($button);
+        $fragment->next_token();
+        $fragment->add_class('the-button');
+
+        return $fragment->get_updated_html();
     }
     add_filter('gform_submit_button', 'my_form_submit_button', 10, 2);
 }
 
 
 // Add second featured image
-add_action( 'add_meta_boxes', 'listing_image_add_metabox' );
-function listing_image_add_metabox () {
-    add_meta_box( 'listingimagediv', __( 'Listing Image', 'text-domain' ), 'listing_image_metabox', 'post', 'side', 'low');
+add_action('add_meta_boxes', 'listing_image_add_metabox');
+function listing_image_add_metabox()
+{
+    add_meta_box('listingimagediv', __('Listing Image', 'text-domain'), 'listing_image_metabox', 'post', 'side', 'low');
 }
 
-function listing_image_metabox ( $post ) {
+function listing_image_metabox($post)
+{
     global $content_width, $_wp_additional_image_sizes;
 
-    $image_id = get_post_meta( $post->ID, '_listing_image_id', true );
+    $image_id = get_post_meta($post->ID, '_listing_image_id', true);
 
     $old_content_width = $content_width;
     $content_width = 254;
 
-    if ( $image_id && get_post( $image_id ) ) {
+    if ($image_id && get_post($image_id)) {
 
-        if ( ! isset( $_wp_additional_image_sizes['post-thumbnail'] ) ) {
-            $thumbnail_html = wp_get_attachment_image( $image_id, array( $content_width, $content_width ) );
+        if (! isset($_wp_additional_image_sizes['post-thumbnail'])) {
+            $thumbnail_html = wp_get_attachment_image($image_id, array($content_width, $content_width));
         } else {
-            $thumbnail_html = wp_get_attachment_image( $image_id, 'post-thumbnail' );
+            $thumbnail_html = wp_get_attachment_image($image_id, 'post-thumbnail');
         }
 
-        if ( ! empty( $thumbnail_html ) ) {
+        if (! empty($thumbnail_html)) {
             $content = $thumbnail_html;
-            $content .= '<p class="hide-if-no-js"><a href="javascript:;" id="remove_listing_image_button" >' . esc_html__( 'Remove listing image', 'text-domain' ) . '</a></p>';
-            $content .= '<input type="hidden" id="upload_listing_image" name="_listing_cover_image" value="' . esc_attr( $image_id ) . '" />';
+            $content .= '<p class="hide-if-no-js"><a href="javascript:;" id="remove_listing_image_button" >' . esc_html__('Remove listing image', 'text-domain') . '</a></p>';
+            $content .= '<input type="hidden" id="upload_listing_image" name="_listing_cover_image" value="' . esc_attr($image_id) . '" />';
         }
 
         $content_width = $old_content_width;
     } else {
 
-        $content = '<img src="" style="width:' . esc_attr( $content_width ) . 'px;height:auto;border:0;display:none;" />';
-        $content .= '<p class="hide-if-no-js"><a title="' . esc_attr__( 'Set listing image', 'text-domain' ) . '" href="javascript:;" id="upload_listing_image_button" id="set-listing-image" data-uploader_title="' . esc_attr__( 'Choose an image', 'text-domain' ) . '" data-uploader_button_text="' . esc_attr__( 'Set listing image', 'text-domain' ) . '">' . esc_html__( 'Set listing image', 'text-domain' ) . '</a></p>';
+        $content = '<img src="" style="width:' . esc_attr($content_width) . 'px;height:auto;border:0;display:none;" />';
+        $content .= '<p class="hide-if-no-js"><a title="' . esc_attr__('Set listing image', 'text-domain') . '" href="javascript:;" id="upload_listing_image_button" id="set-listing-image" data-uploader_title="' . esc_attr__('Choose an image', 'text-domain') . '" data-uploader_button_text="' . esc_attr__('Set listing image', 'text-domain') . '">' . esc_html__('Set listing image', 'text-domain') . '</a></p>';
         $content .= '<input type="hidden" id="upload_listing_image" name="_listing_cover_image" value="" />';
-
     }
 
     echo $content;
 }
 
-add_action( 'save_post', 'listing_image_save', 10, 1 );
-function listing_image_save ( $post_id ) {
-    if( isset( $_POST['_listing_cover_image'] ) ) {
+add_action('save_post', 'listing_image_save', 10, 1);
+function listing_image_save($post_id)
+{
+    if (isset($_POST['_listing_cover_image'])) {
         $image_id = (int) $_POST['_listing_cover_image'];
-        update_post_meta( $post_id, '_listing_image_id', $image_id );
+        update_post_meta($post_id, '_listing_image_id', $image_id);
     }
 }
 
 
-function wpdocs_selectively_enqueue_admin_script( $hook ) {
-    if ( 'post.php' != $hook ) {
+function wpdocs_selectively_enqueue_admin_script($hook)
+{
+    if ('post.php' != $hook) {
         return;
     }
-    wp_enqueue_script( 'secondimage', get_template_directory_uri() . '/assets/secondimage.js', array(), '1.0' );
+    wp_enqueue_script('secondimage', get_template_directory_uri() . '/assets/secondimage.js', array(), '1.0');
 }
-add_action( 'admin_enqueue_scripts', 'wpdocs_selectively_enqueue_admin_script' );
+add_action('admin_enqueue_scripts', 'wpdocs_selectively_enqueue_admin_script');
